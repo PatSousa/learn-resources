604a50ce656cb7d8a3ae607cbdae041dd43be55b
Sousa <sousa@gw20e.com>
2017-10-22 00:48:31 +0200

    oopsie

--- learn_resource/testsettings.py
+++ learn_resource/testsettings.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'travisdb',
-        'USER': 'postgres',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432'

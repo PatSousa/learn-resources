7ba61103aa38b2121e7072832bc4de132ab1499e
Sousa <sousa@gw20e.com>
2017-10-22 14:48:05 +0200

    travis-ci settings

--- settings/tst.py
+++ settings/tst.py
@@ -6,12 +6,14 @@ from .base import *
 Django settings file for you project.
 """
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': os.environ['DATABASE_NAME'],
     'USER': os.environ['USER'],
-    'PASSWORD': os.environ['DATABASE_PASSWORD'],
+    'PASSWORD': os.environ['PASSWORD'],
     }
 }
 

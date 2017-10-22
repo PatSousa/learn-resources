462adcd259fa3e11eafa4bbafc163a6b343d1583
Sousa <sousa@gw20e.com>
2017-10-22 09:59:02 +0200

    oopside

--- settings/tst.py
+++ settings/tst.py
@@ -10,7 +10,7 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': os.environ['DATABASE_NAME'],
-    'USER': os.environ['DATABASE_USER'],
+    'USER': os.environ['USER'],
     'PASSWORD': os.environ['DATABASE_PASSWORD'],
     }
 }
